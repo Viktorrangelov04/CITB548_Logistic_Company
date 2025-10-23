0979ac18166a74c2bac43ac494c9b8d543537182
Viktor Rangelov <viktorrangelov@MacBook-Air.local>
2025-10-24 02:06:41 +0300

    adding login route and cookies

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,37 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import User from "./models/User.js";
+
+app.get("/users"), async (req, res) =>{
+    try{
+        const users = await User.find();
+        res.status(200).json(users);
+    }catch(error){
+        res.status(500).json({message:"Failed to get users"})
+    }
+}
+
+
+app.post("/login", async (req, res)=>{
+    try{
+        const user = await User.findOne({email: req.body.email})
+        if(!user) return res.status(401).json({message: "account with that email doesn't exist"})
+
+        const isMatch = await bcrypt.compare(req.body.password, user.password);
+        if(!isMatch) return res.status(401).json({message:"wrong password"})
+        
+        const token = jwt.sign(
+            { id: user._id, email: user.email},
+            process.env.JWT_SECRET,
+            {expiresIn:"1h"}
+        );
+        console.log("token:", token);
+        res.json({token});
+
+
+    }catch(error){
+        console.error("Login error:", error);
+        res.status(500).json({message:"server error"});
+    }
+});
+
